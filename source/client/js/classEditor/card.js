f9f473fbbde0545decdc7949d06359e8eccd188a
ZitRos <zitros.lab@gmail.com>
2016-03-10 13:19:51 +0200

    Class properties display, sort fix, etc

--- source/client/js/classEditor/card.js
+++ source/client/js/classEditor/card.js
@@ -1,9 +1,19 @@
 import { loadLevel } from "./index";
 
-function block (className) {
-    let el = document.createElement("div");
+function block (className, element = "div") {
+    let el = document.createElement(element);
     el.className = className;
     return el;
+}
+
+function getContainer (prop) {
+    let item = block(`item`),
+        icon = block(`icon public`),
+        text = block(`label`, `span`);
+    item.appendChild(icon);
+    text.textContent = `${ prop["Name"] }: ${ prop["Type"] }`;
+    item.appendChild(text);
+    return item;
 }
 
 /**
@@ -14,13 +24,28 @@ function block (className) {
 export function getCardElement (data) {
     let card = block(`card ${ data["_type"] }`),
         head = block(`head`),
-        headIcon = block(`icon ${ data["ClassType"] || "" }`),
+        headIcon = block(`cardIcon ${ data["ClassType"] || "" }`),
         header = block(`header`);
 
     header.textContent = data["name"];
     head.appendChild(headIcon);
     head.appendChild(header);
     card.appendChild(head);
+
+    let section, body;
+    for (let prop in data["Properties"]) {
+        section = block(`section`);
+        header = block(`header`);
+        header.textContent = "Properties";
+        body = block(`body`);
+        section.appendChild(header);
+        section.appendChild(body);
+        card.appendChild(section);
+        break;
+    }
+    for (let prop in data["Properties"]) {
+        section.appendChild(getContainer(data["Properties"][prop]));
+    }
 
     if (data["_type"] === "package") {
         card.addEventListener("click", () => {
--- source/client/js/classEditor/index.js
+++ source/client/js/classEditor/index.js
@@ -9,11 +9,29 @@ var PATH = "";
  */
 let grid = null;
 
-export function loadLevel (level) {
+function orderData (data) {
+    var sortable = [],
+        sorted = {};
+    for (var property in data)
+        sortable.push([property, data[property]]);
+    sortable.sort(([p1, d1], [p2, d2]) => {
+        if (d1["_type"] === "package" && d2["_type"] !== "package") return -1;
+        if (d2["_type"] === "package" && d1["_type"] !== "package") return 1;
+        if (d1["ClassType"] !== d2["ClassType"]) return d1["ClassType"] > d2["ClassType"] ? 1 : -1;
+        if (p1[0] === "%" && p2[0] !== "%") return -1;
+        if (p2[0] === "%" && p1[0] !== "%") return 1;
+        if (p1[0] === "%" && p2[0] === "%") return p1.substr(1) > p2.substr(1) ? 1 : -1;
+        return p1 > p2 ? 1 : -1;
+    });
+    sortable.forEach(([p, v]) => sorted[p] = v);
+    return sorted;
+}
 
+export function loadLevel (level) {
     PATH = level;
     grid.clear();
     getList("SAMPLES", PATH, (data) => {
+        data = orderData(data);
         for (let obj in data) {
             grid.applyChild(getCardElement(data[obj]));
         }
