339c969026f9c1292b52669786e95a4d31f0f3af
ZitRos <zitros.lab@gmail.com>
2016-03-19 23:02:13 +0200

    Batch file change not to require global gulp dependency anymore

--- import.bat
+++ import.bat
@@ -4,4 +4,4 @@
 :: CHANGE THIS PATH TO YOUR CACHÉ INSTALLATION PATH ON WINDOWS
 set CACHE_DIR=C:\Program Files\InterSystems\Cache20162
 
-gulp & echo w "OK:"_$system.OBJ.ImportDir("%~dp0build\cache",,"ck") halt | "%CACHE_DIR%\bin\cache.exe" -s "%CACHE_DIR%\mgr" -U SAMPLES
+npm run gulp & echo w "OK:"_$system.OBJ.ImportDir("%~dp0build\cache",,"ck") halt | "%CACHE_DIR%\bin\cache.exe" -s "%CACHE_DIR%\mgr" -U SAMPLES
